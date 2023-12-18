ee93e69907b1482ad4f306c81d5d4286cc976cb5
Vladislav Elizarov <velizarovdev@gmail.com>
2023-12-18 18:59:00 +0300

    Add svg as a component via react-native-svg lib only

--- src/navigation/AppNavigation.jsx
+++ src/navigation/AppNavigation.jsx
@@ -3,10 +3,12 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createStackNavigator } from '@react-navigation/stack'
 import { drawerStyles, headerStyles } from './style'
-import MyExercisesScreen from '../screens/MyExercisesScreen';
-import MyTrainingScreen from '../screens/MyTrainingScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import ExerciseScreen from '../screens/ExerciseScreen';
+import MyExercisesScreen from '../screens/MyExercisesScreen'
+import MyTrainingScreen from '../screens/MyTrainingScreen'
+import SettingsScreen from '../screens/SettingsScreen'
+import ExerciseScreen from '../screens/ExerciseScreen'
+import { BookmarkSvg, CogSvg, HumanBarbellSvg } from '../res/svgs'
+import { Theme } from '../styles'
 
 const Drawer = createDrawerNavigator()
 const Stack = createStackNavigator()
@@ -17,14 +19,29 @@ function Root() {
             <Drawer.Screen
                 name='MyExercisesScreen'
                 component={MyExercisesScreen}
+                options={{
+                    drawerIcon: () => <HumanBarbellSvg 
+                                        size={24} 
+                                        fill={Theme.textCommon}/>
+                }}
             />
             <Drawer.Screen
                 name='MyTrainingScreen'
                 component={MyTrainingScreen}
+                options={{
+                    drawerIcon: () => <BookmarkSvg 
+                                        size={24} 
+                                        fill={Theme.textCommon}/>
+                }}
             />
             <Drawer.Screen
                 name='SettingsScreen'
                 component={SettingsScreen}
+                options={{
+                    drawerIcon: () => <CogSvg 
+                                        size={24} 
+                                        fill={Theme.textCommon}/>
+                }}
             />
         </Drawer.Navigator>
     )
---
+++ src/res/svgs/index.js
@@ -0,0 +1,3 @@
+export { default as BookmarkSvg } from './BookmarkSvg.jsx'
+export { default as CogSvg } from './CogSvg.jsx'
+export { default as HumanBarbellSvg } from './HumanBarbellSvg'
