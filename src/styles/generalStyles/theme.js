0b4c03a404623db6d86b808609ea96a5a4dbef0b
Vladislav Elizarov <velizarovdev@gmail.com>
2023-12-18 15:28:56 +0300

    Create screens

---
+++ src/styles/generalStyles/theme.js
@@ -0,0 +1,36 @@
+// ************
+// **** BG ****
+// ************
+const themeBase = '#24323F'
+const themeLevelOne = '#324458'
+// **************
+// **** TEXT ****
+// **************
+const themeTextCommon = '#FFFFFF'
+const themeTextValidationFailing = '#FF355E'
+// **************
+// **** MOOD ****
+// **************
+const themePositive = '#02DAC5'
+const themeAgressive = '#BB86FC'
+const themeRelaxing = '#9195C1'
+// ***************
+// **** OTHER ****
+// ***************
+const themeItemMono = '#5E6D7D'
+const themeItemStereo = '#85929D'
+const themeItemSelf = '#AEB6BE'
+
+
+export {
+    themeBase,
+    themeLevelOne,
+    themeTextCommon,
+    themeTextValidationFailing,
+    themePositive,
+    themeAgressive,
+    themeRelaxing,
+    themeItemMono,
+    themeItemStereo,
+    themeItemSelf,
+}
