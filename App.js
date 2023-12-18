dfac7f47679d2117c3083e8067ae967a3c26247a
Vladislav Elizarov <velizarovdev@gmail.com>
2023-12-18 15:40:54 +0300

    Load font roboto when app started

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+
 import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          'roboto-light': require('./src/res/fonts/roboto/Roboto-Light.ttf'),
+          'roboto-regular': require('./src/res/fonts/roboto/Roboto-Regular.ttf'),
+          'roboto-medium': require('./src/res/fonts/roboto/Roboto-Medium.ttf'),
+          'roboto-bold': require('./src/res/fonts/roboto/Roboto-Bold.ttf'),
+          'roboto-black': require('./src/res/fonts/roboto/Roboto-Black.ttf')
+        });
+      } catch (e) {
+        console.warn(e) // ! important
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="light" />
-      
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        {/* <AppNavigation/> */}
+      </View>
+    </TouchableWithoutFeedback>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="light" />
+    // </View>
   );
 }
 
